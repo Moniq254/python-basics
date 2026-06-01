4d774da3e18fa26ad1bde230b1250346ed7bc3ce
Moniq254 <gatonyemonicanyambura@gmail.com>
2026-06-01 16:27:00 +0300

    first commit

---
+++ input.py
@@ -0,0 +1,7 @@
+#User input allows user to enter data into a Python program
+name=input("Enter your name:" )
+print(name)
+
+age=input("Enter your age: ")
+print(age)
+           
---
+++ operators.py
@@ -0,0 +1,38 @@
+#They are symbols used to perfom operations
+
+#Arithmetic operators -used for calculations
+x=10
+y=5
+
+print(x+y)#addition
+print(x-y)#subtraction
+print(x*y)#multiplication
+print(x/y)#division
+print(x%y) #modulus/reminder
+print(9%5)
+print(x**y) #power
+
+#Comparison operators-used to compare values and then return true or false
+#greater than
+print(5>4)
+print(10>50)
+
+#less than
+print(9<12)
+print(8<4)
+
+#equal to
+print(5==5)
+print(5==10)
+
+#not equal to
+print(4!=5)
+print(7!=7)
+
+#Greater than or equal to
+print(2>=1)
+print(2>=2)
+
+#less than or equal to
+print(5<=7)
+print(5<=5)
